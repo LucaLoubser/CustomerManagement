e3afa449bf9ce456390ce5ad86746c377ea2d1bc
LucaLoubser <lucal@agilebridge.co.za>
2026-08-26 23:23:27 +0200

    Added Customer Entity

---
+++ src/CustomerManagement.Domain/Entities/Customer.cs
@@ -0,0 +1,11 @@
+namespace CustomerManagement.Domain.Entities;
+
+public class Customer
+{
+    public Guid Id {get; set;}
+    public required string FirstName {get; set;}
+    public required string LastName {get; set;}
+    public required string Email {get; set;}
+    public string? PhoneNumber {get; set;}
+    public DateTime CreatedDate {get; set;}
+}
